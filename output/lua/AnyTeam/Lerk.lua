a073815196d02092630851a821a55f36ec230b4c
Nin <therealnin@users.noreply.github.com>
2017-06-30 16:17:22 -0400

    Lerks now cannot fly for a second after getting webbed

--- output/lua/AnyTeam/Lerk.lua
+++ output/lua/AnyTeam/Lerk.lua
@@ -1,3 +1,13 @@
 function Lerk:GetHeartOffset()
     return Vector(0, 0.7, 0)
 end
+
+local oldModifyVelocity = Lerk.ModifyVelocity
+function Lerk:ModifyVelocity(input, velocity, deltaTime)
+    
+    if not self:GetIsWebbed() then
+        oldModifyVelocity(self, input, velocity, deltaTime)
+    else
+        self.gliding = false
+    end
+end
